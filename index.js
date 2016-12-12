a66ab66f974cb8dc6448315c73178fe87fd1ce2d
VuthyVey <vuthy.v@ligercambodia.org>
2016-12-12 15:36:19 +0700

    Debugging

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ var app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function (req, res) {
     res.render('home');
 });
 
-app.listen(3000, function() {
-  console.log('Hi! Vuthy!');
-  console.log('Node app is running on port 3000');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
