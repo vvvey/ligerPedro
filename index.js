05fd2d0d6351f7c483bbb9a0240da0abf7998a22
VuthyVey <vuthy.v@ligercambodia.org>
2016-12-12 15:03:15 +0700

    Add Handlebars to the project

--- index.js
+++ index.js
@@ -1,52 +1,20 @@
-var cool = require('cool-ascii-faces');
 var express = require('express');
+var exphbs  = require('express-handlebars');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+var app = express();
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
+app.listen(3000, function() {
+  console.log('Hi! Vuthy!');
+  console.log('Node app is running on port 3000');
 });
-
-app.get('/times', function(request, response) {
-    var result = ''
-    var times = process.env.TIMES || 5
-    for (i=0; i < times; i++)
-      result += i + ' ';
-  response.send(result);
-});
-
-
-app.get('/vuthy', function(request, response) {
-  response.render('pages/vuthy');
-});
-
-var pg = require('pg');
-
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-});
-
-app.listen(app.get('port'), function() {
-	console.log('Hi! Vuthy!');
-  console.log('Node app is running on port', app.get('port'));
-});
-
