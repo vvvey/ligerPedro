9b5e090c040ca299d750b776382af0d3d3d1e481
abukys <alli.b@ligercambodia.org>
2016-12-13 17:00:07 +0700

    first webpage working

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
+app.get('/welcome', function(req,res){
+	res.render('welcome');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+app.use(express.static('public/'));
