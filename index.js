f5eb0b688380097ba03dc998200df342761875d0
abukys <alli.b@ligercambodia.org>
2016-12-14 15:55:58 +0700

    Added a test database page

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get('/db', function (request, response) {
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       { res.render('db', {results: result.rows} ); }
+       { response.render('db', {results: result.rows} ); }
     });
   });
 });
