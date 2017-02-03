7e542383309ea22a20856a10340becf130593935
VuthyVey <vuthy.v@ligercambodia.org>
2017-02-03 20:19:30 +0700

    Change Date Format

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var app = express();
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -18,7 +18,7 @@ var hbs = exphbs.create({
       return options.inverse(this);
     },
     dateFormat: function(date) {
-      if(date == null) {
+      if (date == null) {
         return;
       }
       var newDate = new Date(date.getTime() + date.getTimezoneOffset() * 60 * 1000);
@@ -27,23 +27,32 @@ var hbs = exphbs.create({
       newDate.setHours(hours - offset);
 
       var seconds = Math.floor((new Date() - newDate) / 1000);
-      
+
       var interval = Math.floor(seconds / 86400); //1 day equal 86400
+      function pad(s) {
+        return (s < 10) ? '0' + s : s;
+      } //change the 1 digit number to 2 digit numbers like 1 --> 01
+      var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
       if (interval >= 2) { //more than two days
-        function pad(s) { return (s < 10) ? '0' + s : s; }//change the 1 digit number to 2 digit numbers like 1 --> 01
-        return [pad(newDate.getDate()), pad(newDate.getMonth()+1), newDate.getFullYear()].join('.');//date format seperated by .
+        return [pad(newDate.getDate()), monthNames[newDate.getMonth()]+',', newDate.getFullYear()].join(' '); //date format seperated by .
       }
       
-      function plural(number, text){
-        if(number == 1){
+
+      function plural(number, text) {
+        if (number == 1) {
           return number + ' ' + text + ' ago';
         } else {
           return number + ' ' + text + 's ago';
         }
       }
-      
+
+      if (interval < 0) {
+        return [pad(newDate.getDate()), monthNames[newDate.getMonth()]+',', newDate.getFullYear()].join(' '); //date format seperated by .
+      }
+
       if (interval >= 1) {
-        return plural(interval,'day')
+        return plural(interval, 'day')
       }
       interval = Math.floor(seconds / 3600); //1 hour equal 3600
       if (interval >= 1) {
@@ -64,16 +73,16 @@ app.set('port', (process.env.PORT || 5000));
 
 // Configure Passport to use Auth0
 var strategy = new Auth0Strategy({
-    domain:       process.env.AUTH0_DOMAIN,
-    clientID:     process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL:  process.env.AUTH0_CALLBACK_URL || 'http://localhost:5000/callback'
-  }, function(accessToken, refreshToken, extraParams, profile, done) {
-    // accessToken is the token to call Auth0 API (not needed in the most cases)
-    // extraParams.id_token has the JSON Web Token
-    // profile has all the information from the user
-    return done(null, profile);
-  });
+  domain: process.env.AUTH0_DOMAIN,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  callbackURL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:5000/callback'
+}, function(accessToken, refreshToken, extraParams, profile, done) {
+  // accessToken is the token to call Auth0 API (not needed in the most cases)
+  // extraParams.id_token has the JSON Web Token
+  // profile has all the information from the user
+  return done(null, profile);
+});
 
 
 passport.use(strategy);
@@ -82,7 +91,7 @@ var routes = require('./routes/index');
 var user = require('./routes/user');
 
 
-//app.use(fake_account);
+app.use(fake_account);
 // This can be used to keep a smaller payload
 passport.serializeUser(function(user, done) {
   done(null, user);
@@ -93,14 +102,18 @@ passport.deserializeUser(function(user, done) {
 });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(session({
   secret: 'shhhhhhhhh',
   resave: true,
   saveUninitialized: true,
-  cookie: { maxAge: 1000 * 60 * 60 }
-  
+  cookie: {
+    maxAge: 1000 * 60 * 60
+  }
+
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -112,7 +125,9 @@ app.listen(app.get('port'), function() {
 
 app.use(express.static('public/'));
 
-app.use(function (req, res) {
+app.use(function(req, res) {
   res.status(400);
-  res.render('notFound', {user: req.user})
+  res.render('notFound', {
+    user: req.user
+  })
 })
