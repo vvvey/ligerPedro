67999165ba0f333db3b9f66d55b7784497aacd68
Vuthy <vuthy.v@ligercambodia.org>
2017-07-06 18:03:02 +0700

    Admin exchange page

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
 
 module.exports.set = function(router, pool) {
+
 	router.get('/admin/transfer_data', ensureLoggedIn, function (req, res) {
 		// req.query.start and req.query.limit is GET request params
 		// e.g. /admin/transfer_data?start=10
@@ -99,4 +100,91 @@ module.exports.set = function(router, pool) {
 			}
 		})		
 	});
+
+	router.get('/admin/exchange_data', ensureLoggedIn, (req, res) => {
+		// req.query.start and req.query.limit is GET request params
+		// e.g. /admin/exchange_data?start=10
+		// e.g. /admin/exchange_data?start=5&limit=10
+
+		
+		// Check if start parms is valid as a number for OFFSET in database
+		// Default: start = 0
+		var start;
+		if (isNaN(req.query.start) || req.query.start == undefined || req.query.start < 0){ 
+	      start = 0 
+	    } else { 
+	      start = parseInt(req.query.start)
+	    } 
+
+		// Check if limit parms is valid as a number for OFFSET in database
+		// Default: limit = 20 meaning that a page would show 20 transfer logs
+	    var limit;
+	    if (isNaN(req.query.limit) || req.query.limit == undefined || req.query.limit < 0) {
+	    	limit = 20;
+	    } else {
+	    	limit = req.query.limit;
+	    }
+
+		var paginateArray = []
+	    pool.query("SELECT count(id) from exchange_list;", (err, result) => {
+	    	var row_number = result.rows[0].count;
+	    	console.log("Row number is " + row_number)
+	    	// Generate array of object based on number of rows, limit and start
+			for (var i = 0; i < Math.ceil(row_number / limit); i++) {
+				paginateArray[i] = {
+					start: i * limit, // parms of link to start (offset)
+					display: i + 1, // number to display
+					active: Math.ceil(start/limit) == i ? true : false // active for CSS
+				}
+			}
+
+			// e.g. paginateArray = 
+			// [ 	{ start: 0, display: 1, active: false },
+			//  	{ start: 20, display: 2, active: true },
+			// 		{ start: 40, display: 3, active: false } ]
+
+	    })
+
+		var query = {
+					text: 	'SELECT exchange_logs.*, exchanger.img_url as exchanger_img_url \
+							FROM exchange_list AS exchange_logs \
+							JOIN account AS exchanger \
+							ON (exchange_logs.email = exchanger.email) ORDER by timecreated DESC \
+							OFFSET $1 LIMIT $2;',
+					values: [start, limit]
+		}
+
+		pool.query(query, (err, result) => {
+			if (err) {
+				res.send(err)
+			} else {
+				console.log(paginateArray)
+				var previousStart; 
+				var nextStart;
+
+				// Is there newer transfer log
+				if (start  == 0) {
+					previousStart = 'none'
+				} else {
+					previousStart = start - limit;
+				}
+
+				// Is there more
+				if (result.rows.length < limit) {
+					nextStart = 'none';
+				} else {
+					nextStart = start + limit;
+				}
+
+				res.render("admin_exchange_logs", {
+					exchange_data: result.rows,
+					previousStart: previousStart, 
+					nextStart: nextStart, 
+					paginations: paginateArray})
+			}
+		}) 
+
+		
+	});
+
 }	
