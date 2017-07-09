b0dc8767f73678c197d3fcc93af6185c71dbd217
SaosuoVisal <visal.s@ligercambodia.org>
2017-07-09 12:31:26 +0700

    Add: giving a valiadtion
    Transfer not correct!

--- routes/transfer.js
+++ routes/transfer.js
@@ -46,21 +46,36 @@ module.exports.set = function(router, pool) {
         text: "SELECT * FROM exchange_list WHERE email = $1 AND pending = 'true' AND type = 'pedro-dollar';",
         values: [email]
       };
+      const getAccountAll = {
+        text: "SELECT * FROM account;"
+      };
+
       pool.query(getAccount, function(accErr, accresult) {
         if(accErr){console.log(accErr);}
         else{
-          console.log("About to query!");
-          pool.query(getExchange, function(exchangeErr, exchangeResult) {
-            if(exchangeErr){console.log(exchangeErr);}
+          pool.query(getAccountAll, function(allAccErr, allAccResult) {
+            if(allAccErr){console.log(allAccErr);}
             else{
-              var moneyExchange = 0;
-              if(exchangeResult.rows){
-                console.log("Something in exhcange");
-                for(var i = 0; i < exchangeResult.rows.length; i++){
-                  moneyExchange += parseFloat(exchangeResult.rows[i].result);
+              var emailsList = [];
+              for(var i = 0; i < allAccResult.rows.length; i++){
+                emailsList.push(allAccResult.rows[i].email);
+              } 
+              console.log("About to query!");
+              pool.query(getExchange, function(exchangeErr, exchangeResult) {
+                if(exchangeErr){console.log(exchangeErr);}
+                else{
+                  var moneyExchange = 0;
+                  if(exchangeResult.rows){
+                    console.log("Something in exhcange");
+                    for(var i = 0; i < exchangeResult.rows.length; i++){
+                      moneyExchange += parseFloat(exchangeResult.rows[i].result);
+                    }
+                  }
+                  console.log("money: " + (accresult.rows[0].budget - moneyExchange));
+
+                  response.render('transfer_personal', {budget: accresult.rows[0].budget - moneyExchange, user: request.user, data: accresult.rows[0].role, emails: emailsList});
                 }
-              }
-              response.render('transfer_personal', {budget: accresult.rows[0].budget - moneyExchange, user: request.user, data: accresult.rows[0].role});
+              });
             }
           });
         }
@@ -160,8 +175,8 @@ module.exports.set = function(router, pool) {
 
     const transferBudget = parseFloat(req.body.amount);
     const senderNewBudget = senderCurrentBudget - transferBudget;
-	const recipientNewBudget = transferBudget + recipientCurrentBudget;
-    
+	  const recipientNewBudget = transferBudget + recipientCurrentBudget;
+    console.log("senderCurrentBudget: " + senderCurrentBudget);
     console.log("transferBudget: " + transferBudget)
     console.log("Sender New Budget: " + senderNewBudget)
     console.log("Recipient New Budget: " + recipientNewBudget)
@@ -171,12 +186,12 @@ module.exports.set = function(router, pool) {
     pool.query("UPDATE account SET budget = $1 WHERE email = $2;", [recipientNewBudget, recipientEmail])
 
     pool.query("INSERT INTO transfer_logs (amount, sender, recipient, sender_resulting_budget, recipient_resulting_budget, date, reason) \
-                VALUES ($1, $2, $3, $4, $5, CURRENT_TIMESTAMP(2), $6)", [transferBudget, senderEmail, recipientEmail, senderNewBudget, recipientNewBudget, reason], function (err, result) {
-                	if (err) {
-                		res.send(err)
-                	} else {
-                		res.redirect('/transfer_personal');
-                	}
-                });
+      VALUES ($1, $2, $3, $4, $5, CURRENT_TIMESTAMP(2), $6)", [transferBudget, senderEmail, recipientEmail, senderNewBudget, recipientNewBudget, reason], function (err, result) {
+    	if (err) {
+    		res.send(err)
+    	} else {
+    		res.redirect('/transfer_personal');
+    	}
     });
+  });
 }
